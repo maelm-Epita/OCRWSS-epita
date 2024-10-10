8d913b8f27689447e4c00ba9c9002177e7bba482
Lepotototor <victor.flament@pm.me>
2024-10-10 16:20:51 +0200

    [ADD] solver, need to fix buffer overflow

--- solver/multi_solver.c
+++ solver/multi_solver.c
@@ -1,3 +1,4 @@
+#include "multi_solver.h"
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -74,14 +75,60 @@ char **get_words(char *path, int *nb_words) {
 list_word *find_all_words(char *grid_path, char **words, size_t nb_words) {
   grid *g = get_grid(grid_path);
   print_grid(g);
-  tree_word *tree = init_tree('\0');
-  for (size_t i = 0; i < nb_words; i++)
-    add_word(tree, words[i]);
+  tree_word *tree = build_from_words(words, nb_words);
+
+  list_word *list = init_list("", 0, 0, 0, 0);
+
+  solve(g, tree, list);
+
   export_tree(tree, "tree");
 
   destroy_tree(&tree);
   destroy_grid(&g);
-  return NULL;
+  list_word *res = list->next;
+  free(list);
+  return res;
+}
+
+void solve(grid *grid, tree_word *tree, list_word *res) {
+  for (int i = 0; i < grid->h; i++) {
+    for (int j = 0; j < grid->w; j++) {
+      tree_word *child = get_child(tree, grid->letters[i * grid->w + j]);
+      if (child != NULL) {
+        for (int d = 0; d < 8; d++) {
+          check(grid, tree, res, i, j, moves[d][0], moves[d][1]);
+        }
+      }
+    }
+  }
+}
+
+void check(grid *grid, tree_word *tree, list_word *res, int i, int j, int i_add,
+           int j_add) {
+  char *substring = malloc(tree_length(tree) + 1);
+  if (substring == NULL)
+    errx(1, "malloc()");
+
+  tree_word *child = get_child(tree, grid->letters[i * grid->w + j]);
+  int s = 0;
+  int x = i + i_add, y = j + j_add;
+
+  while (0 <= x && x <= grid->h && 0 <= y && y <= grid->w && child != NULL) {
+    substring[s] = child->c;
+    s++;
+
+    if (is_leaf(child)) {
+      substring[s] = '\0';
+      remove_word(tree, substring);
+      add_element(res, substring, j, i, y - j_add, x - i_add);
+      child = NULL;
+    }
+
+    if (child != NULL)
+      child = get_child(child, grid->letters[x * grid->w + y]);
+    x += i_add;
+    y += j_add;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -93,8 +140,14 @@ int main(int argc, char *argv[]) {
 
   for (int i = 0; i < nb_words; i++)
     puts(words[i]);
+  putchar('\n');
 
   list_word *res = find_all_words(argv[1], words, nb_words);
+
+  putchar('\n');
+
+  print_list(res);
+  destroy_list(&res);
 
   return 0;
 }
--- solver/multi_solver.h
+++ solver/multi_solver.h
@@ -6,12 +6,17 @@
 
 #include "grid.h"
 #include "list_word.h"
+#include "tree_words.h"
 
 grid *get_grid(char *path);
 
 char **get_words(char *path, int *nb_words);
 
 list_word *find_all_words(char *grid_path, char **words, size_t nb_words);
+
+void solve(grid *grid, tree_word *tree, list_word *res);
+
+void check(grid *grid, tree_word *tree,list_word *res, int i, int j, int i_add, int j_add);
 
 enum directions {
   right,
@@ -23,7 +28,7 @@ enum directions {
   down_left,
   up_left
 };
-static int moves[8][2] = {{1, 0},  {0, 1}, {-1, 0}, {0, -1},
+static const int moves[8][2] = {{1, 0},  {0, 1}, {-1, 0}, {0, -1},
                           {1, -1}, {1, 1}, {-1, 1}, {-1, -1}};
 
 #endif
