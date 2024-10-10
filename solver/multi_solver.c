0d64862e7ef6aec58e4c4839e6d6d7015cbc5e51
Lepotototor <victor.flament@pm.me>
2024-10-10 13:03:55 +0200

    [TEST] grid and tree / [ADD] array moves and directions

--- solver/multi_solver.c
+++ solver/multi_solver.c
@@ -1,21 +1,100 @@
 #include <err.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "grid.h"
+#include "list_word.h"
+#include "tree_words.h"
 
-int main(int argc, char *argv[]){
+grid *get_grid(char *path) {
+  FILE *file = fopen(path, "r");
+
+  if (file == NULL)
+    errx(1, "grid: file not found\n");
+
+  grid *res = init_grid(file);
+  fclose(file);
+  return res;
+}
+
+char **get_words(char *path, int *nb_words) {
+  char **res = NULL;
+
+  FILE *file = fopen(path, "r");
+  if (file == NULL)
+    errx(1, "words: file not found");
+
+  char c;
+  char *word = NULL;
+  int word_size = 0;
+  *nb_words = 0;
+
+  while ((c = fgetc(file)) != EOF) {
+    if (c == '\n') {
+      *nb_words += 1;
+      res = realloc(res, *nb_words * sizeof(char *));
+
+      if (res == NULL)
+        errx(1, "realloc()");
+      word = realloc(word, (word_size + 1) * sizeof(char));
+      if (word == NULL)
+        errx(1, "realloc()");
+
+      word[word_size] = '\0';
+      res[*nb_words - 1] = word;
+      word = NULL;
+      word_size = 0;
+
+    } else {
+      word_size++;
+      word = realloc(word, word_size * sizeof(char));
+      if (word == NULL)
+        errx(1, "realloc()");
+      word[word_size - 1] = c;
+    }
+  }
+
+  if (word_size > 0) {
+    *nb_words += 1;
+    res = realloc(res, *nb_words * sizeof(char *));
+
+    if (res == NULL)
+      errx(1, "realloc()");
+    word = realloc(word, word_size + 1);
+    if (word == NULL)
+      errx(1, "realloc()");
+
+    word[word_size] = '\0';
+    res[*nb_words - 1] = word;
+  }
+
+  return res;
+}
+
+list_word *find_all_words(char *grid_path, char **words, size_t nb_words) {
+  grid *g = get_grid(grid_path);
+  print_grid(g);
+  tree_word *tree = init_tree('\0');
+  for (size_t i = 0; i < nb_words; i++)
+    add_word(tree, words[i]);
+  export_tree(tree, "tree");
+
+  destroy_tree(&tree);
+  destroy_grid(&g);
+  return NULL;
+}
+
+int main(int argc, char *argv[]) {
   if (argc != 3)
     errx(1, "usage: ./multi_solver <grid_file> <words_file>");
 
-  FILE *grid_file = fopen(*(argv + 1), "r");
+  int nb_words = -1;
+  char **words = get_words(argv[2], &nb_words);
 
-  if (grid_file == NULL)
-    errx(1, "grid: file not found\n");
+  for (int i = 0; i < nb_words; i++)
+    puts(words[i]);
 
-  grid *g = init_grid(grid_file);
-  fclose(grid_file);
+  list_word *res = find_all_words(argv[1], words, nb_words);
 
-  print_grid(g);
-  
   return 0;
 }
--- solver/multi_solver.h
+++ solver/multi_solver.h
@@ -1,6 +1,29 @@
 #ifndef MULTI_SOLVER_H
 #define MULTI_SOLVER_H
 
-#include "solver.h"
+#include <err.h>
+#include <stdlib.h>
+
+#include "grid.h"
+#include "list_word.h"
+
+grid *get_grid(char *path);
+
+char **get_words(char *path, int *nb_words);
+
+list_word *find_all_words(char *grid_path, char **words, size_t nb_words);
+
+enum directions {
+  right,
+  down,
+  left,
+  up,
+  up_right,
+  down_right,
+  down_left,
+  up_left
+};
+static int moves[8][2] = {{1, 0},  {0, 1}, {-1, 0}, {0, -1},
+                          {1, -1}, {1, 1}, {-1, 1}, {-1, -1}};
 
 #endif
