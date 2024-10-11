a23a3bb7e33d99159f2284c5123aca5d01b01e29
mael <mael.minard@epita.fr>
2024-10-11 22:39:25 +0200

    load image into input

---
+++ training-sets/training_data.h
@@ -0,0 +1,14 @@
+struct training_data{
+  float* inputs;
+  float expected_output;
+};
+
+struct training_set{
+  size_t input_number;
+  size_t data_number;
+  struct training_data* data;
+};
+
+struct training_set create_training_set(float** inputs, float* outputs, size_t data_number, size_t input_number);
+void free_training_set(struct training_set set);
+void print_training_set(struct training_set set);
---
+++ training-sets/training_data_loader.h
@@ -0,0 +1,6 @@
+float* image_to_input(char* path);
+float* alpha_to_ouput(char letter);
+float get_dataset_size();
+float get_inputs_size();
+float get_outputs_size();
+void load_training_data(float** *inputpointer, float** *outputpointer);
