ec6056e97ec8650cb651e3bb62726883f51f1c00
mael <mael.minard@epita.fr>
2024-10-12 08:28:25 +0200

    debut data format et loading

--- train/training_data_loader.c
+++ train/training_data_loader.c
@@ -1,6 +1,21 @@
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_pixels.h>
 #include <SDL2/SDL_image.h>
+#include <stdio.h>
+
+// helper
+char to_upper(char c){
+  if (c>='a' && c<='z'){
+    c = c + ('A'-'a');
+  }
+  return c;
+}
+
+// property ish
+size_t get_outputs_size(){
+  return 26;
+}
+
 
 float* image_to_input(char* path){
   SDL_Surface *img = IMG_Load(path);
@@ -19,4 +34,15 @@ float* image_to_input(char* path){
     *(input+i) = greyscale;
   }
   return input;
+}
+
+float* alpha_to_output(char letter){
+  letter = to_upper(letter);
+  float* output = calloc(get_outputs_size(), sizeof(float));
+  *(output+(letter-'A')) = 1; 
+  return output;
+}
+
+void load_training_data(FILE file, float** inputpointer, float** outputpointer){
+
 }
--- train/training_data_loader.h
+++ train/training_data_loader.h
@@ -3,4 +3,4 @@ float* alpha_to_ouput(char letter);
 float get_dataset_size();
 float get_inputs_size();
 float get_outputs_size();
-void load_training_data(float** *inputpointer, float** *outputpointer);
+void load_training_data(FILE file, float** *inputpointer, float** *outputpointer);
