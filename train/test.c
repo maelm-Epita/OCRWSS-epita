2069d9243014bda5dfcef0f1c59db78afeaf526d
mael <mael.minard@epita.fr>
2024-10-13 19:22:40 +0200

    adapted loading function

--- train/test.c
+++ train/test.c
@@ -13,20 +13,24 @@ void free_multi_arr(float** arr, size_t size){
 int main(){
   float **inputs;
   float **outputs;
-  size_t data_nb;
-  size_t input_size;
+  const size_t data_nb = 372038;
+  const size_t input_size = 28*28;
   FILE *fptr;
-  fptr = fopen("./test-set/data.d", "r");
+  fptr = fopen("./training-set/uncompressed/handwritten_data_785.csv", "r");
   if (fptr == NULL){
     printf("File could not be opened");
     exit(EXIT_FAILURE);
   }
-  load_training_data(fptr, &inputs, &outputs, &data_nb, &input_size);
+  printf("Loading training data...\n");
+  load_training_data(fptr, &inputs, &outputs, data_nb, input_size);
   fclose(fptr);
+  printf("Creating training set...\n");
   struct training_set letter_training_set = create_training_set(inputs, outputs, data_nb, input_size);
   free(inputs);
   free(outputs);
+  printf("------------\n");
   print_training_set(letter_training_set);
+  printf("------------\n");
   free_training_set(letter_training_set);
   return 0;
 }
--- train/training_data_loader.h
+++ train/training_data_loader.h
@@ -3,4 +3,4 @@ float* alpha_to_ouput(char letter);
 float get_dataset_size();
 float get_inputs_size();
 float get_outputs_size();
-void load_training_data(FILE *file, float** *inputs_p, float** *outputs_p, size_t *data_number, size_t *input_number);
+void load_training_data(FILE *file, float** *inputs_p, float** *outputs_p, size_t data_number, size_t input_size);
